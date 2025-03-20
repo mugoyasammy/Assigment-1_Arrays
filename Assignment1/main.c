f3d4d3d6af018b5a3927d28d1ef12301c3450035
mugoyasammy <mugoyamuwanga@gmail.com>
2025-03-20 11:42:47 +0300

    Commit#5
    putting Commits

--- Assignment1/main.c
+++ Assignment1/main.c
@@ -63,12 +63,13 @@ scanf("%d",&x);
 char Wild[x][6];
 char Merged[n+x][13];
     int i,j;
-
+// Prompting the user to enter the Domestic animals.
 printf("Please Enter the Domestic animals");
 printf("\n");
 for(int i =0;i<n;i++){
     scanf("%s",&Domestic[i]);
 }
+// Prompting the user to enter the Wild animals.
 printf("\n\n");
 printf("Please enter the Wild animals");
 printf("\n");
@@ -76,14 +77,16 @@ for( j =0;j<x;j++){
     scanf("%s",&Wild[j]);
 }
 //printf("\n\n");
-
+// Copying Domestic animals into merged array.
 for (int i =0;i<n+x;i++){
     strcpy(Merged[i],Domestic[i]);
 }
+// Copying wild animals into merged array.
      printf("\n");
 for (int j=0;j<n+x;j++){
     strcpy(Merged[n+j],Wild[j]);
 }
+// Printing Merged array
      printf("\n");
 for (int i = 0;i<n+x;i++){
 printf("%s\n",Merged[i]);}
